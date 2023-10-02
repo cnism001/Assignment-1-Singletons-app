061104e5bf1ed168249d5e3a7c7c778754ff9226
cnism001 <cnism001@edu.xamk.fi>
2023-10-02 20:10:11 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Singletons_app
+{
+    
+
+    //Singleton class
+    class Logger
+    {
+        //private static variable for singular instance of class Logger, since there can'be be more
+        //than one instance of the  Singleton pattern class 
+        static private Logger Instance;
+
+        //Private constructor, preventing instantiation from outside of Logger class, creates new Logs list when Instance is
+        //first instantiated
+        private Logger()
+        {
+            Logs = new List<string>();
+        }
+
+        //Method,returning singular instance of Logger class, creating instance if it didn't exist and then
+        //returning it or returning existing instance
+        public static Logger GetInstance()
+        {
+
+
+            if (Instance == null)
+            {
+                Instance = new Logger();
+            }
+            return Instance;
+
+        }
+        // List Logs, storing the logged messages, set to private to prevent changes done from outside of Logger class
+        public List<string> Logs { get; private set; }
+
+        // Public method TextLog,adds the string to the Logs list
+        public void TextLog(string message)
+        {
+            Logs.Add(message);
+            Console.WriteLine(message);
+        }
+
+    }
+
+}
